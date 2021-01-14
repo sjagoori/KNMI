81e488df3c1bb067eadf14fecfa8fe73fca3f529
dewarian <nathanbommezijn@gmail.com>
2021-01-14 14:17:23 +0100

    First draft of the information component

---
+++ components/information/information.js
@@ -0,0 +1,66 @@
+import { Component } from 'react'
+import { render } from 'react-dom';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme'
+import Icon from 'assets/svg/help'
+
+export default class Information extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <InfoContainer>
+                        <Title>{this.props.title}</Title>
+                        <Icon />
+                        <InfoDescription>{this.props.infoDescription}</InfoDescription>
+                        <InfoDescription>{this.props.infoDescriptionSecondary}</InfoDescription>
+                    </InfoContainer>
+                    <ImageContainer>
+                        <Image src={this.props.imgUrl} />
+                    </ImageContainer>
+                </Container>
+            </ThemeProvider>
+        )
+    }
+}
+
+const Container = styled.div`
+// background-color: ${props => props.theme.colors.white};
+    background-color: #ffcc00;
+    width: 95%;
+    height: 450px;
+
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    margin: 40px;
+    padding: 15px;
+`
+const InfoContainer = styled.div`
+    margin: 0 auto;
+    margin-left: 1em;
+    max-width: 50%;
+    display: inline-flex;
+    flex-direction: column;
+`
+
+const Title = styled.h2`
+    font-size: 2em;
+`
+
+const InfoDescription = styled.p`
+    opacity: 70%;
+`
+const ImageContainer = styled.div`
+    margin: auto 20px;
+    order: 1;
+`
+const Image = styled.img`
+    width: 100%;
+    height: 
+`
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Information from '@/components/information/information'
 import Navbar from 'components/navbar/Navbar'
 
 export default function Home() {
@@ -10,6 +11,13 @@ export default function Home() {
           link3: { label: 'Link', url: '#' },
         }}
         cta={{ label: 'label', url: '#' }}
+      />
+      <Information
+        title='NO₂ uitstoot'
+        infoDescription='NO₂ is a chemical compound that has been used in fertilizers, but also is a product of burning fossil oils e.g. motor vehicles or stoves. NO₂ is a chemical compound that has been used in fertilizers, but also is a product of burning fossil oils e.g. motor vehicles or stoves.
+        PM25 is fine particles on a specific size'
+        infoDescriptionSecondary='PM25 is a fine particle matter'
+        imgUrl='https://images.unsplash.com/photo-1581626221838-9715c0e06049?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'
       />
     </>
   )
