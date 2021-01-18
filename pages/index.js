a17d22e3c59ad49a7a26075ec56917781e987d00
sjagoori <sjagoori@users.noreply.github.com>
2021-01-18 11:40:17 +0100

    included map component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 import Navbar from 'components/navbar/Navbar'
 import mapboxgl from 'mapbox-gl'
+import Map from 'components/mapchart/Map'
 
 export default class Home extends Component {
   constructor(props) {
@@ -8,18 +9,30 @@ export default class Home extends Component {
   }
 
   componentDidMount() {
-    new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [4.895168, 52.370216],
-      zoom: 5
-    });
+    // new mapboxgl.Map({
+    //   container: 'map',
+    //   style: 'mapbox://styles/mapbox/streets-v11',
+    //   center: [4.895168, 52.370216],
+    //   zoom: 5
+    // });
   }
+
 
   render() {
     return (
       <>
-        <div id='map'></div>
+        {/* <div id='map'></div> */}
+
+        <Map
+          id='container'
+          title='Title'
+          lead='description'
+          // primarySet='https://example.com/dataset.json'
+          // secondarySet='https://example.com/dataset.json' //optional
+          // mapData='https://cartomap.github.io/nl/wgs84/gemeente_2020
+          // mapData='https://gist.githubusercontent.com/MariellaCC/0055298b94fcf2c16940/raw/8b145cbcef92e1431446cf87fb278283750fa3b1/ne_50m_admin_0_countries_simplified.json'
+          // filterOptions={['option1', 'option2']} //optional
+        />
       </>
     )
   }
