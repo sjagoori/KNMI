a0787dceb9acf9f94f690d64741e9277dd6b09dd
sjagoori <sjagoori@users.noreply.github.com>
2021-01-19 19:40:52 +0100

    + added dataset & adjusted map

--- components/mapchart/composer.js
+++ components/mapchart/composer.js
@@ -1,31 +1,32 @@
 import { select, geoMercator, geoPath, selectAll, json } from 'd3';
 import { generateHeadBlock, handleFilter, handleMouseOver, handleMouseOut, handleMouseMove } from './util';
-const mapData = require('assets/mapdata.json')
-export const projection = geoMercator().scale(650).center([25, 60])
-
+const mapData = require('assets/AQICN_data_coords.json')
+export const projection = geoMercator().scale(700).center([15, 63])
 
 export function composer(data) {
   //Width and height
-  var w = 700;
-  var h = 700;
+  var w = 900;
+  var h = 800;
 
   //Define path generator
   const path = geoPath().projection(projection)
 
+  let container = select("#" + data.chartId).append('div')
+
   //Create SVG
-  var svg = select("#" + data.chartId)
+  var map = container
     .append("svg")
     .attr("width", w)
     .attr("height", h);
 
   //Bind data and create one path per GeoJSON feature
-  svg.selectAll("path")
+  map.selectAll("path")
     .data(mapData.features)
     .enter()
     .append("path")
     .attr("d", path)
-    .attr("stroke", "#FFF")
-    .attr("fill", "#000")
+    .style("stroke", "#000")
+    .style("fill", d => d.properties.continent == 'Europe' ? ("#ff0000" + Math.round(d.properties.delta * 100)) : '')
     .attr("country", d=> d.properties.admin)
     .attr("continent", d=> d.properties.continent)
     .on("mouseover", handleMouseOver)
