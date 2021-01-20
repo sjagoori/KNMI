f696eda331fdd0be2299c84a8f83ec9beaf66dfe
sjagoori <sjagoori@users.noreply.github.com>
2021-01-20 14:35:40 +0100

    + included hero

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Navbar from 'components/navbar/Navbar'
+import Hero from 'components/hero/Hero'
 
 export default function Home() {
   return (
@@ -10,6 +11,16 @@ export default function Home() {
           link3: { label: 'Link', url: '#' },
         }}
         cta={{ label: 'label', url: '#' }}
+      />
+
+      <Hero
+        imgUrl='https://images.unsplash.com/photo-1454117096348-e4abbeba002c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D'
+        title='What effect has the coronavirus on our NO² emission?'
+        lead='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus.'
+        label='something'
+        url='#'
+        buttonLabel='What is NO²'
       />
     </>
   )
