e360b3afe3ae67186929752471842ae82189c64a
sjagoori <sjagoori@users.noreply.github.com>
2021-01-21 02:56:12 +0100

    ~typos

--- pages/index.js
+++ pages/index.js
@@ -29,9 +29,9 @@ export default function Home() {
 
       <Information
         id='about'
-        title='First of all, what is Nitrogen Dioxide (NO₂)?'
-        infoDescription='Nitrogen Dioxide (NO₂) is one of the highly reactive gases and gets primarily in the air from the burning of fuel. It forms from emissions from gas-stoves, cars, trucks and buses, power plants, and off-road equipment.'
-        infoDescriptionSecondary='Breathing air with high concentration of NO₂ can irritate airways in the human repiratory system. Short exposure can aggravate patients with respiratory diseases. Longer exposures to elevated concentratinos of NO₂ may contribute to the development of asthma.'
+        title='First of all, what is Nitrogen Dioxide (NO²)?'
+        infoDescription='Nitrogen Dioxide (NO²) is one of the highly reactive gases and gets primarily in the air from the burning of fuel. It forms from emissions from gas-stoves, cars, trucks and buses, power plants, and off-road equipment.'
+        infoDescriptionSecondary='Breathing air with high concentration of NO² can irritate airways in the human repiratory system. Short exposure can aggravate patients with respiratory diseases. Longer exposures to elevated concentratinos of NO² may contribute to the development of asthma.'
         imgUrl='https://i.ibb.co/Kz5s0KT/tropomi-map-NO2.jpg'
         sources={{
           source1: { url: 'https://aqicn.org/scale', label: 'AQICN severity levels (20-01-2021)' },
