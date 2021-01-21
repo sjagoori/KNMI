42e3721e4c2acd40dc9df74a5d62e1bf207576cd
sjagoori <sjagoori@users.noreply.github.com>
2021-01-21 02:57:12 +0100

    ~ more typos

--- pages/index.js
+++ pages/index.js
@@ -21,17 +21,17 @@ export default function Home() {
         id='into'
         imgUrl='https://images.unsplash.com/photo-1566386576349-695cb12288f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'
         title='What effect has the coronavirus on our Nitrogen Dioxide emission?'
-        description='Throughout this pandemic we have seen alot, but we also have seen something extraordinary, our NO² got significantly less. But how much?'
+        description='Throughout this pandemic we have seen alot, but we also have seen something extraordinary, our NO₂ got significantly less. But how much?'
         label='Introduction'
         url='#about'
-        buttonLabel='What is NO²'
+        buttonLabel='What is NO₂'
       />
 
       <Information
         id='about'
-        title='First of all, what is Nitrogen Dioxide (NO²)?'
-        infoDescription='Nitrogen Dioxide (NO²) is one of the highly reactive gases and gets primarily in the air from the burning of fuel. It forms from emissions from gas-stoves, cars, trucks and buses, power plants, and off-road equipment.'
-        infoDescriptionSecondary='Breathing air with high concentration of NO² can irritate airways in the human repiratory system. Short exposure can aggravate patients with respiratory diseases. Longer exposures to elevated concentratinos of NO² may contribute to the development of asthma.'
+        title='First of all, what is Nitrogen Dioxide (NO₂)?'
+        infoDescription='Nitrogen Dioxide (NO₂) is one of the highly reactive gases and gets primarily in the air from the burning of fuel. It forms from emissions from gas-stoves, cars, trucks and buses, power plants, and off-road equipment.'
+        infoDescriptionSecondary='Breathing air with high concentration of NO₂ can irritate airways in the human repiratory system. Short exposure can aggravate patients with respiratory diseases. Longer exposures to elevated concentratinos of NO₂ may contribute to the development of asthma.'
         imgUrl='https://i.ibb.co/Kz5s0KT/tropomi-map-NO2.jpg'
         sources={{
           source1: { url: 'https://aqicn.org/scale', label: 'AQICN severity levels (20-01-2021)' },
